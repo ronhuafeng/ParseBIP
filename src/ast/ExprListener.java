f80d53608c4beb7a0ad39b9978db4ab9c5d24d28
ronhuafeng <ron.huafeng@gmail.com>
2014-11-28 15:45:02 +0800

    将项目上传到 Github

---
+++ src/ast/ExprListener.java
@@ -0,0 +1,230 @@
+// Generated from E:\Tsmart Projects\ParseBIP\resource\Expr.g4 by ANTLR 4.1
+package ast;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link ExprParser}.
+ */
+public interface ExprListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#inclusive_or_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterInclusive_or_expression(@NotNull ExprParser.Inclusive_or_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#inclusive_or_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitInclusive_or_expression(@NotNull ExprParser.Inclusive_or_expressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#ObjectExpression}.
+	 * @param ctx the parse tree
+	 */
+	void enterObjectExpression(@NotNull ExprParser.ObjectExpressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#ObjectExpression}.
+	 * @param ctx the parse tree
+	 */
+	void exitObjectExpression(@NotNull ExprParser.ObjectExpressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#assignment_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterAssignment_expression(@NotNull ExprParser.Assignment_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#assignment_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitAssignment_expression(@NotNull ExprParser.Assignment_expressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#multiplicative_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterMultiplicative_expression(@NotNull ExprParser.Multiplicative_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#multiplicative_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitMultiplicative_expression(@NotNull ExprParser.Multiplicative_expressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#relational_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterRelational_expression(@NotNull ExprParser.Relational_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#relational_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitRelational_expression(@NotNull ExprParser.Relational_expressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#FunctionCall}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunctionCall(@NotNull ExprParser.FunctionCallContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#FunctionCall}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunctionCall(@NotNull ExprParser.FunctionCallContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#exclusive_or_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterExclusive_or_expression(@NotNull ExprParser.Exclusive_or_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#exclusive_or_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitExclusive_or_expression(@NotNull ExprParser.Exclusive_or_expressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#statement}.
+	 * @param ctx the parse tree
+	 */
+	void enterStatement(@NotNull ExprParser.StatementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#statement}.
+	 * @param ctx the parse tree
+	 */
+	void exitStatement(@NotNull ExprParser.StatementContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#if_then_else_expression_compatible}.
+	 * @param ctx the parse tree
+	 */
+	void enterIf_then_else_expression_compatible(@NotNull ExprParser.If_then_else_expression_compatibleContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#if_then_else_expression_compatible}.
+	 * @param ctx the parse tree
+	 */
+	void exitIf_then_else_expression_compatible(@NotNull ExprParser.If_then_else_expression_compatibleContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#logical_and_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterLogical_and_expression(@NotNull ExprParser.Logical_and_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#logical_and_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitLogical_and_expression(@NotNull ExprParser.Logical_and_expressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#additive_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterAdditive_expression(@NotNull ExprParser.Additive_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#additive_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitAdditive_expression(@NotNull ExprParser.Additive_expressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#if_then_else_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterIf_then_else_expression(@NotNull ExprParser.If_then_else_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#if_then_else_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitIf_then_else_expression(@NotNull ExprParser.If_then_else_expressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#do_action}.
+	 * @param ctx the parse tree
+	 */
+	void enterDo_action(@NotNull ExprParser.Do_actionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#do_action}.
+	 * @param ctx the parse tree
+	 */
+	void exitDo_action(@NotNull ExprParser.Do_actionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#equality_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterEquality_expression(@NotNull ExprParser.Equality_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#equality_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitEquality_expression(@NotNull ExprParser.Equality_expressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#primary_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterPrimary_expression(@NotNull ExprParser.Primary_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#primary_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitPrimary_expression(@NotNull ExprParser.Primary_expressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#and_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterAnd_expression(@NotNull ExprParser.And_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#and_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitAnd_expression(@NotNull ExprParser.And_expressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#unary_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterUnary_expression(@NotNull ExprParser.Unary_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#unary_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitUnary_expression(@NotNull ExprParser.Unary_expressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#logical_or_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterLogical_or_expression(@NotNull ExprParser.Logical_or_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#logical_or_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitLogical_or_expression(@NotNull ExprParser.Logical_or_expressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#subtractive_expression}.
+	 * @param ctx the parse tree
+	 */
+	void enterSubtractive_expression(@NotNull ExprParser.Subtractive_expressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#subtractive_expression}.
+	 * @param ctx the parse tree
+	 */
+	void exitSubtractive_expression(@NotNull ExprParser.Subtractive_expressionContext ctx);
+
+	/**
+	 * Enter a parse tree produced by {@link ExprParser#argument_expression_list}.
+	 * @param ctx the parse tree
+	 */
+	void enterArgument_expression_list(@NotNull ExprParser.Argument_expression_listContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ExprParser#argument_expression_list}.
+	 * @param ctx the parse tree
+	 */
+	void exitArgument_expression_list(@NotNull ExprParser.Argument_expression_listContext ctx);
+}
